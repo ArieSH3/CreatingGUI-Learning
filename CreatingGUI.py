4785a0e56268ab6ad05b893bfd264f583b93053f
ArieSH3 <StepDubUp@gmail.com>
2021-05-16 18:13:29 +0200

    CreatingGUI.py

---
+++ CreatingGUI.py
@@ -0,0 +1,11 @@
+'''
+	Author: Stefan Popovic
+	Date  : 16/05/2021
+
+	Resources: pyautogui documentation, tkinter documentation
+	
+	About: Learning to create a GUI with Python libraries and possibly create
+		   applications and full user interfaces for software.
+
+	TODO: ----
+'''
