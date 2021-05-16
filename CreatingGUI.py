101f01e49f3f887180690432fb99c37d3238e0a3
ArieSH3 <StepDubUp@gmail.com>
2021-05-16 19:48:35 +0200

    Adding popup window

--- CreatingGUI.py
+++ CreatingGUI.py
@@ -15,17 +15,37 @@
 #import tkinter
 import PySimpleGUI as sg
 
-sg.theme('DarkAmber') 
+def main():
 
-layout = [	[sg.Text('Some text on Row 1')],
-			[sg.Text('Enter somethng on Row 2'), sg.InputText()],
-			[sg.Button('Ok'), sg.Button('Cancel')]	]
+	popup()
+	someWindow()
 
-window = sg.Window('Window Title', layout)
 
-while True:
-	event, values = window.read()
-	if event == sg.WIN_CLOSED or event == 'Cancel':
-		break
-	print('You entered ', values[0])
-	#aa
+
+
+
+# Window asking for text and displaying it
+def someWindow():
+	sg.theme('DarkAmber') 
+
+	layout = [	[sg.Text('Some text on Row 1')],
+				[sg.Text('Enter somethng on Row 2'), sg.InputText()],
+				[sg.Button('Ok'), sg.Button('Cancel')]	]
+
+	window = sg.Window('Window Title', layout)
+
+	while True:
+		event, values = window.read()
+		if event == sg.WIN_CLOSED or event == 'Cancel':
+			break
+		print('You entered ', values[0])
+
+# Popup window with a custom message
+def popup():
+	sg.theme('SystemDefault')
+	sg.popup('ERROR: 0x4FA1F', 'Your PC is shutting down in 1 minute...')
+
+
+
+
+main()
