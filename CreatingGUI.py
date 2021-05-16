dbfcdd2ea137db211e93ff5bc3b20e10daf451dc
ArieSH3 <StepDubUp@gmail.com>
2021-05-16 18:43:29 +0200

    Creating simple window

--- CreatingGUI.py
+++ CreatingGUI.py
@@ -11,12 +11,20 @@
 	TODO: ----
 '''
 
-import pyautogui
-import tkinter
-import pysimplegui as sg
+#import pyautogui
+#import tkinter
+import PySimpleGUI as sg
 
 sg.theme('DarkAmber') 
 
 layout = [	[sg.Text('Some text on Row 1')],
-			[sg.Text('Enter somethng on Row 2')],
+			[sg.Text('Enter somethng on Row 2'), sg.InputText()],
 			[sg.Button('Ok'), sg.Button('Cancel')]	]
+
+window = sg.Window('Window Title', layout)
+
+while True:
+	event, values = window.read()
+	if event == sg.WIN_CLOSED or event == 'Cancel':
+		break
+	print('You entered ', values[0])
