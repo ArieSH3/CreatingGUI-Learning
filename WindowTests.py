be5cf03909e18a799c1c458efb02071b30cf4a51
ArieSH3 <StepDubUp@gmail.com>
2021-05-17 21:44:59 +0200

    Added new file, indepth window creation

---
+++ WindowTests.py
@@ -0,0 +1,24 @@
+'''
+	Author: Stefan Popovic
+	Date  : 17/05/2021
+
+	About : More in-dept modification of windows and its elements
+
+'''
+
+import PySimpleGUI as sg
+
+
+def main():
+
+
+
+
+
+
+
+
+
+
+
+main()
